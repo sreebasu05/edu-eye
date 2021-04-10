ad74c1aa73d5c103db8e8f98bf02c640500cc122
gursimran18 <kaurgursimran9918@gmail.com>
2021-04-10 21:17:31 +0530

    user roles based permissions set

--- student/views.py
+++ student/views.py
@@ -2,92 +2,121 @@ from django.contrib import messages
 from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
 from django.views import generic
 from django.views.generic.base import View
-
+from django.contrib.auth.decorators import login_required
 from .forms import StudentProfileForm
 from account.models import *
 
-
+@login_required(login_url='login')
 def home_student(request):
-    return render(request, 'student/home.html')
-
-def student_profile(request):
-    if(StudentProfile.objects.filter(student=request.user).exists()):
+    if request.user.is_student == True :
         return render(request, 'student/home.html')
     else:
-        if request.method == 'POST':
-            fm = StudentProfileForm(request.POST)
-            if fm.is_valid():
-                instance = fm.save(commit=False)
-                instance.student = request.user
-                print(instance.id)
-                # teacher_id=instance.id                #num = Batch.objects.filter(in_class=request.POST.get('class'), section=request.POST.get('section')).count()
+        print("NOT A Student!!")
+        return redirect('login')
 
-                # if(num == 0)
-                #     Batch.objects.create(                #         in_class=request.POST.get('class'),
-            #         section=request.POST.get('section')
-                #     ).save()
-                #
-                # instance.batch = Batch.objects.get(in_class=request.POST.get('class'), section=request.POST.get('section'))
-
-
-                instance.save()
-                messages.success(request, 'Your profile has been created')
-                return render(request,'student/home.html')
-                    # return render(request,'teacher/home.html')
-            else:
-                messages.error(request, 'Please enter valid details')
-                return render(request,'student/profile-form.html')
+@login_required(login_url='login')
+def student_profile(request):
+    if request.user.is_student == True :
+        if(StudentProfile.objects.filter(student=request.user).exists()):
+            return render(request, 'student/home.html')
         else:
-            fm=StudentProfileForm()
-            print("hello")
-            return render(request,'student/profile-form.html',{'form':fm})
+            if request.method == 'POST':
+                fm = StudentProfileForm(request.POST)
+                if fm.is_valid():
+                    instance = fm.save(commit=False)
+                    instance.student = request.user
+                    print(instance.id)
+                    # teacher_id=instance.id                #num = Batch.objects.filter(in_class=request.POST.get('class'), section=request.POST.get('section')).count()
+
+                    # if(num == 0)
+                    #     Batch.objects.create(                #         in_class=request.POST.get('class'),
+                #         section=request.POST.get('section')
+                    #     ).save()
+                    #
+                    # instance.batch = Batch.objects.get(in_class=request.POST.get('class'), section=request.POST.get('section'))
 
 
+                    instance.save()
+                    messages.success(request, 'Your profile has been created')
+                    return render(request,'student/home.html')
+                        # return render(request,'teacher/home.html')
+                else:
+                    messages.error(request, 'Please enter valid details')
+                    return render(request,'student/profile-form.html')
+            else:
+                fm=StudentProfileForm()
+                print("hello")
+                return render(request,'student/profile-form.html',{'form':fm})
+
+
+    else:
+        print("NOT A Student!!")
+        return redirect('login')
+
+@login_required(login_url='login')
 def dash(request):
     uid = request.user.id
     print(request.user.email)
-    student = StudentProfile.objects.get(student=User.object.get(id=uid))
-    print(student.first_name)
-    courses = BatchCourse.objects.filter(batch=student.student_batch)
-    context = {
-        'courses': courses,
-        'student':student,
-    }
-    return render(request, 'student/student_dash.html', context)
-
-def profile(request):
-    student = student = StudentProfile.objects.get(student=User.object.get(id=request.user.id))
-    context = {
-        'student': student
-    }
-
-    return render(request, 'student/profile.html', context)
-
-
-def units(request, bid):
-    batches = TrackProgressBatchCourse.objects.filter(batchcourse_id=bid)
-    b = BatchCourse.objects.get(pk=bid)
-    print(bid)
-    return render(request, 'student/unit.html', {'batches': batches, 'batch':b})
-
-
-def giveRating(request,bid):
-    track = TrackProgressBatchCourse.objects.get(pk=bid)
-
-    print(track.batchcourse)
-
-    if request.method == 'POST':
-        rating = request.POST['rating']
-        
-        print(rating)
-        
-        track.rating = track.rating + int(rating)
-        track.students_polled =track.students_polled+1
-        track.save()
-            
-        return render(request,'student/home.html')
+    if request.user.is_student == True :
+        student = StudentProfile.objects.get(student=User.object.get(id=uid))
+        print(student.first_name)
+        courses = BatchCourse.objects.filter(batch=student.student_batch)
+        context = {
+            'courses': courses,
+            'student':student,
+        }
+        return render(request, 'student/student_dash.html', context)
     else:
-        print("hello")
-        return render(request,'student/unit.html',{'bid':bid})
+        print("NOT A Student!!")
+        return redirect('login')
 
-    
+@login_required(login_url='login')
+def profile(request):
+    if request.user.is_student == True :
+        student = student = StudentProfile.objects.get(student=User.object.get(id=request.user.id))
+        context = {
+            'student': student
+        }
+
+        return render(request, 'student/profile.html', context)
+    else:
+        print("NOT A Student!!")
+        return redirect('login')
+
+
+
+@login_required(login_url='login')
+def units(request, bid):
+    if request.user.is_student ==True:
+        batches = TrackProgressBatchCourse.objects.filter(batchcourse_id=bid)
+        b = BatchCourse.objects.get(pk=bid)
+        print(bid)
+        return render(request, 'student/unit.html', {'batches': batches, 'batch':b})
+    else:
+        print("NOT AUTHORIZED!!")
+        return redirect('login')
+@login_required(login_url='login')
+def giveRating(request,bid):
+    if request.user.is_student ==True:
+
+        track = TrackProgressBatchCourse.objects.get(pk=bid)
+
+        print(track.batchcourse)
+
+        if request.method == 'POST':
+            rating = request.POST['rating']
+            
+            print(rating)
+            
+            track.rating = track.rating + int(rating)
+            track.students_polled =track.students_polled+1
+            track.save()
+                
+            return render(request,'student/home.html')
+        else:
+            print("hello")
+            return render(request,'student/unit.html',{'bid':bid})
+
+    else:
+        print("NOT AUTHORIZED!!")
+        return redirect('login')
--- teacher/views.py
+++ teacher/views.py
@@ -1,74 +1,105 @@
 from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
 from .forms import TeacherProfileForm
 from account.models import *
-
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 # Create your views here.
 
 
-
+@login_required(login_url='login')
 def home_teacher(request):
-    return render(request, 'teacher/home.html')
-
-def TeachProfile(request):
-    currUser = request.user.email
-    Teacher = get_object_or_404(TeacherProfile, teacher_id=request.user.id)
-    return render(request, 'teacher/profile.html',{'Teacher':Teacher})
-
-
-def CreateProfile(request):
-    if(TeacherProfile.objects.filter(teacher=request.user).exists()):
+    if request.user.is_teacher ==True:
         return render(request, 'teacher/home.html')
-    if request.method == 'POST':
-        fm = TeacherProfileForm(request.POST)
-        if fm.is_valid():
-            instance = fm.save(commit=False)
-            instance.teacher = request.user
-            print(instance.id)
-            # teacher_id=instance.id
-            instance.save()
-            messages.success(request, 'Your profile has been created')
-            return render(request,'teacher/profileform.html')
-            # return render(request,'teacher/home.html')
+    else:
+        print("NOT A Teacher!! login as teacher :P")
+        return redirect('login')
+
+@login_required(login_url='login')
+def TeachProfile(request):
+    if request.user.is_teacher ==True:
+        currUser = request.user.email
+        Teacher = get_object_or_404(TeacherProfile, teacher_id=request.user.id)
+        return render(request, 'teacher/profile.html',{'Teacher':Teacher})
+    else:
+        print("NOT A Teacher!! login as teacher :P")
+        return redirect('login')
+
+@login_required(login_url='login')
+def CreateProfile(request):
+    if request.user.is_teacher ==True:
+        if(TeacherProfile.objects.filter(teacher=request.user).exists()):
+            return render(request, 'teacher/home.html')
+        if request.method == 'POST':
+            fm = TeacherProfileForm(request.POST)
+            if fm.is_valid():
+                instance = fm.save(commit=False)
+                instance.teacher = request.user
+                print(instance.id)
+                # teacher_id=instance.id
+                instance.save()
+                messages.success(request, 'Your profile has been created')
+                return render(request,'teacher/profileform.html')
+                # return render(request,'teacher/home.html')
+            else:
+                messages.error(request, 'Please enter valid details')
+                return render(request,'teacher/home.html')
         else:
-            messages.error(request, 'Please enter valid details')
-            return render(request,'teacher/home.html')
-    else:
-        fm=TeacherProfileForm()
-        print("hello")
-        return render(request,'teacher/profileform.html',{'form':fm})
+            fm=TeacherProfileForm()
+            print("hello")
+            return render(request,'teacher/profileform.html',{'form':fm})
 
+    else:
+        print("NOT A Teacher!! login as teacher :P")
+        return redirect('login')
+
+@login_required(login_url='login')
 def dash(request):
-    uid = request.user.id
-    Teacher = get_object_or_404(TeacherProfile, teacher_id=uid)
-    Teaches = TeacherBatchCourse.objects.filter(teacher_details=Teacher)
-    return render(request,'teacher/dashboard.html',{'Teaches':Teaches})
-    print(Teach)
-
-def unitdetail(request, bid):
-    batchdetail = TrackProgressBatchCourse.objects.filter(batchcourse_id=bid)
-    print(batchdetail)
-    # print(batchdetail.unit.unit_no)
-    # print(batchdetail.batchcourse)
-    # print(batchdetail)
-
-    return render(request, 'teacher/unitdash.html', {'batchdetails': batchdetail,'bid':bid})
-    
-def isCompleteUint(request, bid, unitid):
-    batchdetail = TrackProgressBatchCourse.objects.get(batchcourse_id=bid,unit_id=unitid)
-    if request.method == 'POST':
-        comp = request.POST['comp']
-        lecture = request.POST['lecture']
-        print(comp)
-        print(lecture)
-        if comp == "Completed":
-            batchdetail.is_completed = True
-
-        batchdetail.lecture_taken = lecture
-        print(batchdetail)
-        batchdetail.save()
-            
-        return render(request,'teacher/home.html')
+    if request.user.is_teacher ==True:
+        uid = request.user.id
+        Teacher = get_object_or_404(TeacherProfile, teacher_id=uid)
+        Teaches = TeacherBatchCourse.objects.filter(teacher_details=Teacher)
+        return render(request,'teacher/dashboard.html',{'Teaches':Teaches})
+        print(Teach)
     else:
-        print("hello")
-        return render(request,'teacher/unitform.html',{'bid':bid,'unitid':unitid})
+        print("NOT A Teacher!! login as teacher :P")
+        return redirect('login')
+
+@login_required(login_url='login')
+def unitdetail(request, bid):
+    if request.user.is_teacher ==True:
+        if request.user.is_teacher ==True:
+            batchdetail = TrackProgressBatchCourse.objects.filter(batchcourse_id=bid)
+            print(batchdetail)
+            # print(batchdetail.unit.unit_no)
+            # print(batchdetail.batchcourse)
+            # print(batchdetail)
+
+        return render(request, 'teacher/unitdash.html', {'batchdetails': batchdetail,'bid':bid})
+    else:
+        print("NOT A Teacher!! login as teacher :P")
+        return redirect('login')
+
+        
+@login_required(login_url='login')
+def isCompleteUint(request, bid, unitid):
+    if request.user.is_teacher ==True:
+        batchdetail = TrackProgressBatchCourse.objects.get(batchcourse_id=bid,unit_id=unitid)
+        if request.method == 'POST':
+            comp = request.POST['comp']
+            lecture = request.POST['lecture']
+            print(comp)
+            print(lecture)
+            if comp == "Completed":
+                batchdetail.is_completed = True
+
+            batchdetail.lecture_taken = lecture
+            print(batchdetail)
+            batchdetail.save()
+                
+            return render(request,'teacher/home.html')
+        else:
+            print("hello")
+            return render(request,'teacher/unitform.html',{'bid':bid,'unitid':unitid})
+    else:
+        print("NOT A Teacher!! login as teacher :P")
+        return redirect('login')
