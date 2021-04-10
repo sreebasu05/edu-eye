3e67b9824935d24308ab729f56dbd57b29a025ec
S P Ramya Pratyusha <spr.pratyusha2001@gmail.com>
2021-04-10 16:11:42 +0530

    Routes corrected

--- student/views.py
+++ student/views.py
@@ -10,44 +10,38 @@ from account.models import *
 def home_student(request):
     return render(request, 'student/home.html')
 
-def StudProfile(request):
-    currUser = request.user.email
-    student = get_object_or_404(StudentProfile, student_id=request.user.id)
-    return render(request, 'student/profile.html',{'Student':student})
-
 
 def student_profile(request):
-    mail = request.user.email
-    print(mail)
-    if request.method == 'POST':
-        fm = StudentProfileForm(request.POST)
-        if fm.is_valid():
-            instance = fm.save(commit=False)
-            instance.student = request.user
-            print(instance.id)
-            # teacher_id=instance.id
-            #num = Batch.objects.filter(in_class=request.POST.get('class'), section=request.POST.get('section')).count()
-
-            # if(num == 0):
-            #     Batch.objects.create(
-            #         in_class=request.POST.get('class'),
-            #         section=request.POST.get('section')
-            #     ).save()
-            #
-            # instance.batch = Batch.objects.get(in_class=request.POST.get('class'), section=request.POST.get('section'))
-
-
-            instance.save()
-            messages.success(request, 'Your profile has been created')
-            return render(request,'student/profile.html')
-            # return render(request,'teacher/home.html')
-        else:
-            messages.error(request, 'Please enter valid details')
-            return render(request,'student/home.html')
+    if(StudentProfile.objects.filter(student=request.user).exists()):
+        return render(request, 'student/home.html')
     else:
-        fm=StudentProfileForm()
-        print("hello")
-        return render(request,'student/profile-form.html',{'form':fm})
+        if request.method == 'POST':
+            fm = StudentProfileForm(request.POST)
+            if fm.is_valid():
+                instance = fm.save(commit=False)
+                instance.student = request.user
+                print(instance.id)
+                # teacher_id=instance.id                #num = Batch.objects.filter(in_class=request.POST.get('class'), section=request.POST.get('section')).count()
+
+                # if(num == 0)
+                #     Batch.objects.create(                #         in_class=request.POST.get('class'),
+            #         section=request.POST.get('section')
+                #     ).save()
+                #
+                # instance.batch = Batch.objects.get(in_class=request.POST.get('class'), section=request.POST.get('section'))
+
+
+                instance.save()
+                messages.success(request, 'Your profile has been created')
+                return render(request,'student/home.html')
+                    # return render(request,'teacher/home.html')
+            else:
+                messages.error(request, 'Please enter valid details')
+                return render(request,'student/profile-form.html')
+        else:
+            fm=StudentProfileForm()
+            print("hello")
+            return render(request,'student/profile-form.html',{'form':fm})
 
 
 def dash(request):
